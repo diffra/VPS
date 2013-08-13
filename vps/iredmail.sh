c2f18dd2053e7878ad1ba0136c5eeccf1064faa7
James Bertelson <james@bertelson.me>
2013-08-13 10:25:05 -0700

    Fixed case issues with iRedMail

--- vps/iredmail.sh
+++ vps/iredmail.sh
@@ -7,7 +7,7 @@ apt_install 'bzip2'
 wget -O /tmp/iredmail.tar.bz2 https://bitbucket.org/zhb/iredmail/downloads/iRedMail-0.8.5.tar.bz2  
 cd /tmp
 tar xjvf iredmail.tar.bz2
-bash iRedMail-0.8.5/iredmail.sh
+bash iRedMail-0.8.5/iRedMail.sh
 }
 
 addModule "iredmail (Full mail solution)"
